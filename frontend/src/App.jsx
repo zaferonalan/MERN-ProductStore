5319973ba5f602a0b30d5afe90b699c3966a3957
zafergunayonalan <zafergunay7@gmail.com>
2025-04-10 21:01:36 +0300

    navbar-created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';
 import Navbar from './components/Navbar';
+import { useColorModeValue } from './components/ui/color-mode';
 
 function App() {
 
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,49 @@
+import { Button, Container, Flex, HStack, Text} from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { CiSquarePlus } from "react-icons/ci";
+import { useColorMode } from './ui/color-mode';
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <div>Navbar</div>
+    <Container maxW={"1140px"} px={4}>
+			<Flex
+				h={16}
+				alignItems={"center"}
+				justifyContent={"space-between"}
+				flexDir={{
+					base: "column",
+					sm: "row",
+				}}
+			>
+				<Text
+					fontSize={{ base: "22px", sm: "28px" }}
+					fontWeight={"bold"}
+					textTransform={"uppercase"}
+					textAlign={"center"}
+					bgGradient={"to-r"}
+                    gradientFrom="cyan.400"
+                    gradientTo="blue.500"
+					bgClip={"text"}
+				>
+					<Link to={"/"}>Product Store 🛒</Link>
+				</Text>
+
+				<HStack spacing={2} alignItems={"center"}>
+					<Link to={"/create"}>
+						<Button>
+							<CiSquarePlus fontSize={20} />
+						</Button>
+					</Link>
+					<Button onClick={toggleColorMode} >
+						{colorMode === "light" ? <IoMoon /> : <LuSun size='20' /> }
+					</Button>
+				</HStack>
+			</Flex>
+	</Container>
   )
 }
 
