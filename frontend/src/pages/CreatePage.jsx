05703bb1e6471583dcb2ab8ab5b880e5d0845b67
zafergunayonalan <zafergunay7@gmail.com>
2025-04-11 16:22:05 +0300

    createPageUI-created

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import { useColorModeValue } from '@/components/ui/color-mode'
+import { Box, Button, Container, Heading, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const CreatePage = () => {
+    const [newProduct, setNewProduct] = useState({
+        name:"",
+        price:"",
+        image:"",
+    })
+
+    const handleAddProduct = () => {
+        console.log(newProduct)
+    }
+
   return (
-    <div>CreatePage</div>
+    <Container maxW={"container.sm"}>
+			<VStack spacing={8}>
+				<Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+					Create New Product
+				</Heading>
+
+				<Box w={"full"} bg={useColorModeValue("white", "gray.800")} p={6} rounded={"lg"} shadow={"md"}>
+					<VStack spacing={4}>
+						<Input
+							placeholder='Product Name'
+							name='name'
+                            value={newProduct.name}
+                            onChange={(e) => setNewProduct({...newProduct, name:e.target.value})}
+						/>
+						<Input
+							placeholder='Price'
+							name='price'
+							type='number'
+                            value={newProduct.price}
+                            onChange={(e) => setNewProduct({...newProduct, price:e.target.value})}
+						/>
+						<Input
+							placeholder='Image URL'
+							name='image'
+                            value={newProduct.image}
+                            onChange={(e) => setNewProduct({...newProduct, image:e.target.value})}
+						/>
+
+						<Button colorScheme='blue' w='full' onClick={handleAddProduct}>
+							Add Product
+						</Button>
+					</VStack>
+				</Box>
+			</VStack>
+		</Container>
   )
 }
 
